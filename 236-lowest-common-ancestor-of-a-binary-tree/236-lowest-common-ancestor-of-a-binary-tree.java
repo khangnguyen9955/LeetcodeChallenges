7d4c91bc9ff3acbf4df9e1e2d89c70a2efa935e6
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-08 22:13:15 +0700

    Time: 15 ms (20.98%), Space: 47.3 MB (57.43%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,11 +8,12 @@
  * }
  */
 class Solution {
+    // flag when we found p or q
         boolean foundP= false, foundQ= false;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         // Pattern: When we found the p || q at a root => go to left and right to find the rest
-                if(root == null) return null;
+         if(root == null) return null;
 
         if(root.val == q.val){
             foundQ = true;
@@ -24,13 +25,17 @@ class Solution {
         }
         
        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        // if we found p and q at left subtree, just return it !! 
         if(foundQ && foundP){
             return left;
         }
-            TreeNode right = lowestCommonAncestor(root.right,p,q);
-       if(left != null && right != null){
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        //if we have not found p and q at left subtree, it must be in right or root.
+        
+       if(left != null && right != null){ // if left and right is not null, it must be the root
            return root;
        }
+        // but if the left or the right is null, it must be the rest.
         if(left == null){
             return right;
         }else {
