2a28d98763f4516c96da0871db0e7f754b783ded
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-08 22:11:16 +0700

    Time: 9 ms (68.90%), Space: 46.9 MB (80.09%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,20 +8,34 @@
  * }
  */
 class Solution {
+        boolean foundP= false, foundQ= false;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         // Pattern: When we found the p || q at a root => go to left and right to find the rest
-        if(root== q|| root == p|| root == null){
+                if(root == null) return null;
+
+        if(root.val == q.val){
+            foundQ = true;
             return root;
         }
-           TreeNode left = lowestCommonAncestor(root.left,p,q);
-            TreeNode right = lowestCommonAncestor(root.right,p,q);
-        if(left == null){
-            return right;
-        }else if(right == null){
+        if(root== q|| root == p|| root == null){
+            foundP = true;
+            return root;
+        }
+        
+       TreeNode left = lowestCommonAncestor(root.left,p,q);
+        if(foundQ && foundP){
             return left;
         }
-        else{
-            return root;
+            TreeNode right = lowestCommonAncestor(root.right,p,q);
+       if(left != null && right != null){
+           return root;
+       }
+        if(left == null){
+            return right;
+        }else {
+            return left;
         }
+        
     }
 }
