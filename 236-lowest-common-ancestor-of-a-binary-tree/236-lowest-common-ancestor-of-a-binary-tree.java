3b7dc8408ac1452a118a9edc217b186e20026a06
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-08 22:18:20 +0700

    Time: 12 ms (36.80%), Space: 48.1 MB (15.33%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -26,11 +26,12 @@ class Solution {
            return root;
        }
         // but if the left or the right is null, it must be the rest.
-        if(left == null){
+        /* if(left == null){
             return right;
         }else {
             return left;
-        }
+        }*/
+        return left == null ? right : left;
         
     }
 }
