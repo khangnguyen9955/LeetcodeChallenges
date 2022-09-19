a6b9924add6880f862061ad6168cf26883d6e1c0
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-19 21:11:42 +0700

    Time: 10 ms (86.51%), Space: 69.4 MB (76.14%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -3,20 +3,23 @@ class Solution {
         int max =  mat[0].length + mat.length;
         for(int i = 0 ; i < mat.length;i++){
             for(int j = 0 ; j < mat[0].length;j++){
-                if(mat[i][j] == 0) continue;
+                if(mat[i][j] == 0 ) continue;
                   int a = max, c = max;
                   if(i>0 ) a = mat[i-1][j];
                   if(j>0) c=mat[i][j-1];
                   mat[i][j] = Math.min(a,c) + 1;
+                
             }
         }
         for(int i = mat.length-1 ; i>=0;i--){
             for(int j = mat[0].length-1 ; j >=0 ;j--){
-                if(mat[i][j] == 0) continue;
+             if(mat[i][j] == 0) continue;
                     int b=max,d=max;
                       if(i<mat.length-1) b=mat[i+1][j]; 
                       if(j<mat[0].length-1) d = mat[i][j+1]; 
                       mat[i][j] = Math.min(Math.min(b,d) + 1,mat[i][j]);
+                 
+             
             }
         }
         
