c2132840f248372f861a4b3c79205077a481991c
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-21 21:10:57 +0700

    Time: 44 ms (46.31%), Space: 43.6 MB (34.00%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -19,32 +19,25 @@ class Node {
 */
 
 class Solution {
-    public Node cloneGraph(Node node) {
-        // create a hashmap to store all the pairs, avoid duplicate neighbor pair
-      HashMap<Integer,Node> map = new HashMap<>();
-     return helper(node,map);
-    }
-    
-   public Node helper(Node node, HashMap<Integer,Node> map){
-       
-       if(node == null) return null;
-       // contains => return that pair
-       if(map.containsKey(node.val))
-       {
-           return map.get(node.val);
-       }
-       else{
+      // create a hashmap to store all the pairs, avoid duplicate neighbor pair
+        HashMap<Node,Node> map = new HashMap<>();
+     public Node cloneGraph(Node node) {
+        if(node ==null) return node;
+          // contains => return that pair
+        if(map.containsKey(node)){
+            return map.get(node);
+        }
+        Node newGraph= new Node(node.val, new ArrayList());
            // clone here
            // create new graph with the node.val (1) 
-           Node newGraph= new Node(node.val);
-           // put it into the hashmap
-           map.put(node.val,newGraph);
-           // for each neighbor in the node
-           for(Node neighbor : node.neighbors){
-               // add its neighbor pair into this graph
-               newGraph.neighbors.add(helper(neighbor,map));
-           }
-        return newGraph;    
-       }
-   }
+        map.put(node,newGraph);
+         // put it into the new graph
+        for(Node neighbor: node.neighbors){
+                // add its neighbor pair into this graph
+            newGraph.neighbors.add(cloneGraph(neighbor));
+        }
+        return newGraph;
+    }
+    
+
 }
