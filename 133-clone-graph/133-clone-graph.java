a9a0cfbd9461d6985dee21c5b71b3df6d1ab6ff0
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-21 21:12:59 +0700

    Time: 48 ms (27.48%), Space: 43.3 MB (53.91%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -1,48 +1,16 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
+    private HashMap<Node,Node> visited= new HashMap<>();
     public Node cloneGraph(Node node) {
-        // create a hashmap to store all the pairs, avoid duplicate neighbor pair
-      HashMap<Integer,Node> map = new HashMap<>();
-     return helper(node,map);
+        if(node ==null) return node;
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+        
+        Node cloneNode= new Node(node.val, new ArrayList());
+        visited.put(node, cloneNode);
+        for(Node neighbor: node.neighbors){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
     }
-   public Node helper(Node node, HashMap<Integer,Node> map){
-       if(node == null) return null;
-       // contains => return that pair
-       if(map.containsKey(node.val))
-       {
-           return map.get(node.val);
-       }
-       else{
-           // clone here
-           // create new graph with the node.val (1) 
-           Node newGraph= new Node(node.val);
-           // put it into the hashmap
-           map.put(node.val,newGraph);
-           // for each neighbor in the node
-           for(Node neighbor : node.neighbors){
-               // add its neighbor pair into this graph
-               newGraph.neighbors.add(helper(neighbor,map));
-           }
-        return newGraph;    
-       }
-   }
 }
