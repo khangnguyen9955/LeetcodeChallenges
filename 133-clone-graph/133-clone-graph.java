4fb9921a820d739c46dd159dfed8a159047dc762
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-09-21 21:06:12 +0700

    Time: 49 ms (23.61%), Space: 43.6 MB (34.00%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -20,20 +20,28 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
+        // create a hashmap to store all the pairs, avoid duplicate neighbor pair
       HashMap<Integer,Node> map = new HashMap<>();
      return helper(node,map);
     }
     
    public Node helper(Node node, HashMap<Integer,Node> map){
+       
        if(node == null) return null;
+       // contains => return that pair
        if(map.containsKey(node.val))
        {
            return map.get(node.val);
        }
        else{
+           // clone here
+           // create new graph with the node.val (1) 
            Node newGraph= new Node(node.val);
+           // put it into the hashmap
            map.put(node.val,newGraph);
+           // for each neighbor in the node
            for(Node neighbor : node.neighbors){
+               // add its neighbor pair into this graph
                newGraph.neighbors.add(helper(neighbor,map));
            }
         return newGraph;    
