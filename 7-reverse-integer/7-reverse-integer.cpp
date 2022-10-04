cf05be6e878bbfea250d39d7c73447d46cc29df0
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-10-04 18:33:50 +0700

    Time: 0 ms (100.00%), Space: 5.9 MB (75.29%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+    long temp =0;
+    long pos = 0;
+    long result= 0;
+        while (x!=0){
+        pos = x % 10;           
+        x/=10;
+       if (temp > INT_MAX/10 || (temp == INT_MAX / 10 && pos > 7)) return 0;
+            if (temp < INT_MIN/10 || (temp == INT_MIN / 10 && pos < -8)) return 0;
+        temp = result * 10 + pos;
+        result = temp;
+        }
+      
+         return result;
+    }
+};
