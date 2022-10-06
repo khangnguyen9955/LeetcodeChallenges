148668b2fe1cb9ab26ed15d23cfc9db3e2049089
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-10-06 13:57:34 +0700

    Time: 7 ms (53.97%), Space: 46.3 MB (30.16%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -5,33 +5,33 @@ class Solution {
         backtracking(result,0,n, new LinkedList<String>(), new int[2*n] , new int[2*n], new int[2*n]);
         return result;
     }
-    
-    
-    
     public void backtracking(List<List<String>> result, int col, int n, List<String> path, int[] ld, int [] rd, int [] cl ){
        if(col >= n){
+           // full=> return.
            result.add(new LinkedList<>(path));
            return;
        }
-       
         for(int row = 0 ; row < n;row++){
+            // row-col+n-1 means left diagonal, write it down to the paper with some position examples will understand
+            // row+col means right diagonal
+            // cl[row] this row cannot be same
+            // ensure that not same col because we call col+1
             if(ld[row-col+n-1] != 1 && rd[row+col] != 1 && cl[row] != 1){
-               ld[row-col+n-1] =1;
-                rd[row+col]=1;
+               ld[row-col+n-1] =
+                rd[row+col]=
                 cl[row]=1;
+                // now this pos is possible to placed the Queen, append it and recursive
                 StringBuilder str = new StringBuilder();
                 for(int i =0 ;i<n;i++) str.append('.');
                 str.setCharAt(row,'Q');
                 path.add(str.toString());
                 backtracking(result,col+1,n,path,ld,rd,cl);
-                ld[row-col+n-1]=0;
-                rd[row+col]=0;
+                // now the path above is not possible, backtrack and remove it out of the list paths
+                ld[row-col+n-1]=
+                rd[row+col]=
                 cl[row]=0;
                 path.remove(path.size()-1);
-                
             }
-            
-            
         }
     }
 }
