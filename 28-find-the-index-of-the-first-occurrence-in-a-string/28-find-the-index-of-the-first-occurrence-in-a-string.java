cffd1117e576e86e22363454f6c63043d8018341
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-10-09 14:01:04 +0700

    Time: 2 ms (19.56%), Space: 41.9 MB (55.12%) - LeetHub

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0){
+            return 0;
+        }
+        if (needle.length() > haystack.length()){
+            return -1;
+        }
+        if(haystack.length() == 0) return -1;
+        
+        for (int i =0;i<haystack.length();i++){
+            if((i + needle.length()) > haystack.length() ) break;   
+            for (int j = 0;j<needle.length();j++){
+                    if(needle.charAt(j) != haystack.charAt(i+j)) break;
+                    if(j == needle.length()-1) return i;
+                }
+            }   
+       return -1;
+       
+        
+        
+        
+        
+        
+    }
+}
