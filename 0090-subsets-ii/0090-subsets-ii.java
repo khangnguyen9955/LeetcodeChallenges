fa23ca58e511a20962dea453c13eae08f9ed9c79
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-10-21 16:37:51 +0700

    Time: 2 ms (87.17%), Space: 44.2 MB (48.05%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>(); 
+        Arrays.sort(nums);
+        backtrack(list,new ArrayList<>(), nums,0);
+        return list;        
+    }
+    
+    public void backtrack(List<List<Integer>> list ,  List<Integer> tempList, int[] nums,int start){
+        list.add(new ArrayList<>(tempList));
+            for(int i =start ; i< nums.length; i++){
+            if (i == start || nums[i] != nums[i - 1]){ // tranh truong hop duplicate , neu i khac start, co nghia la i da chay den het array roi, remove va backtrack len di
+                tempList.add(nums[i]);
+            backtrack(list,tempList,nums,i+1);
+            tempList.remove(tempList.size()-1);}
+        }
+        
+        
+        
+        
+    }
+}
