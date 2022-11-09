2fd4ba466b0195a754d47b7e0e4c4311cfd8c8df
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-11-09 20:53:09 +0700

    Time: 12 ms (44.09%), Space: 46.1 MB (34.96%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String intToRoman(int num) {
+        if(num < 1 || num > 3999){
+            return "";
+        }
+        // we have to take care about the case with number "4*" and "9*"
+        // so I think we should add some more symbol
+        // IV, IX, XL, XC, CD, CM
+        String[] symbols = { "M", "CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        StringBuilder str = new StringBuilder();
+        int i=0; // iterate variable
+        while(num > 0){ // check if done
+            while(num >= values[i]){
+                num -= values[i]; 
+                str.append(symbols[i]);
+                // for example: num=2400 => num=1400 => num = 400 
+                // then we have 2 M
+                // now num is less than M (1000) => increase i by 1;
+            }
+            i++;
+        }
+        return str.toString();
+    }
+}
