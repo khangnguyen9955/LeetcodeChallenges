0603eb3e3671f63fdc35955fe9661f15624f128e
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-11-20 18:26:54 +0700

    Time: 7 ms (98.53%), Space: 6 MB (32.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool isPalindrome(int x) {
+	bool result = true;
+	int a = x;
+	int b = 0;
+	int d = 0;
+	if (x < 0)
+		return false;
+	if (x > 0) {
+		while (x != 0) {
+			b = x % 10;
+			x /= 10;
+			if (d > INT_MAX / 10 || (d == INT_MAX / 10 && b > 7)) return 0;
+			if (d < INT_MIN / 10 || (d == INT_MIN / 10 && b < -8)) return 0;
+			d = d * 10 + b;
+		};
+		if (d == a) {
+			result = true;
+		}
+		else result = false;
+	}
+	return result;
+
+}
+};
