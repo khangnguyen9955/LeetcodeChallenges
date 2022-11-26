6fbb2af171def72f3ec2bf1b265fd99fd2dfaa07
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-11-26 10:39:04 +0700

    Time: 1 ms (85.31%), Space: 42.4 MB (7.00%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+         // set = {1,2,3,4,5,6,7,8,9}
+        // take it as an array 
+        // we got k numbers can use in this set;
+        // each number in the list can be used only once
+        // we can create a boolean[] flag to check the above condition
+        // with the process check to choose next num
+        // i think we can implement the binarysearch to use (?) instead of linear search
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res,new ArrayList<>(),n,new boolean[10],1,k);
+        return res;
+    }
+    
+    public void backtrack(List<List<Integer>> res, List<Integer> list, int remain, boolean[]flag, int start, int k){
+        if(remain < 0){
+            return;
+        }
+        else if (remain ==0){
+            if(k == list.size()){
+            res.add(new ArrayList<>(list));
+            }
+        }
+        else{
+            while(start < 10 && start <= remain){
+            list.add(start);
+            backtrack(res,list,remain - start, flag, start+1,k);
+            list.remove(list.size()-1);
+                start++;
+            }
+        }
+    }
+}
