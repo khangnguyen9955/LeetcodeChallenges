2e434f40b1bdb45db6181c766c76b98dc335c01c
Khang Nguyen <38356205+khangnguyen9955@users.noreply.github.com>
2023-05-23 09:09:39 +0700

    Time: 0 ms (100.00%), Space: 39.6 MB (67.76%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -2,10 +2,10 @@ class Solution {
     public int kthFactor(int n, int k) {
        
         
-// first loop is a straightforward when want to find factors
+// first loop is a straightforward when want to find factors just go to sqrt of n
 // for example: n = 12
         // sqrt(n)=3,4...
-        // loop from 1 to 3
+        // loop from 1 to <3
        for(int i = 1; i < Math.sqrt(n); i++){
            if(n%i==0 && --k ==0){
                return i;
