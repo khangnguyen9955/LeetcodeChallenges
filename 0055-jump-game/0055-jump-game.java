da25568f367a882104cb8d8753b9c413340546dd
Khang Nguyen <38356205+khangnguyen9955@users.noreply.github.com>
2023-06-20 10:11:42 +0700

    Time: 1 ms (99.98%), Space: 43.6 MB (63.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        /*
         int cur = nums[0];
         for(int i = 1; i<nums.length;i++){
             if(cur < i) return false;
@@ -9,6 +10,16 @@ class Solution {
             if(cur >= nums.length) return true;
         }
         return true;
+        */
+        
+        int res = nums[nums.length-1];
+        for(int i =nums.length-1;i>=0;i--){
+            if(nums[i] + i >= res){
+                res = i;
+            }
+        }
+        if(res == 0) return true;
+        return false;
     }
 
 
